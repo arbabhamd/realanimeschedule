43f6a0efafb25e71201db1838a429c3a9d7532c9
arbabhamd <arbabhamd@gmail.com>
2023-05-28 22:10:09 +0500

    feat: committed data

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <!-- <img alt="realanimeschedule" class="logo" src="@/assets/logo.svg" width="100" height="50" /> -->
+    <ul>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </ul>
+    <button class="btn btn-primary">Test</button>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  background-color: #333;
+  padding: 1rem;
+  width: 100vw;
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+}
+
+ul {
+  display: flex;
+  list-style: none;
+  justify-content: space-between;
+  margin: 0;
+  padding: 0;
+}
+</style>
